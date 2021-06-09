95e2f99edcebf07e3229319490858374ea5e29cf
alex-skxy <alex13skx@gmail.com>
2021-06-09 13:09:47 +0200

    CreateController

--- app/Controllers/CreateController.php
+++ app/Controllers/CreateController.php
@@ -4,9 +4,27 @@ class CreateController
 {
     public function index()
     {
-        $hello = 'Create';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->create($_POST);
+        } else {
+            require 'app/Views/create.view.php';
+        }
+    }
 
-        require 'app/Views/create.view.php';
+
+    public function create($data)
+    {
+        $pdo = db();
+        $statement = $pdo->prepare("INSERT INTO loan (name, lastname, email, phone_number, installments, fk_creditpackage_id) VALUES (:name, :lastname, :phone_number, :phone_number, :installments, :creditpackage_id);");
+        $statement->bindParam(':name', $_POST['name']);
+        $statement->bindParam(':lastname', $_POST['lastname']);
+        $statement->bindParam(':email', $_POST['email']);
+        $statement->bindParam(':phone_number', $_POST['phone_number']);
+        $statement->bindParam(':installments', $_POST['installments']);
+        $statement->bindParam(':creditpackage_id', $_POST['creditpackage']);
+
+        $statement->execute();
+        $result = $statement->fetchAll();
     }
 }
 
