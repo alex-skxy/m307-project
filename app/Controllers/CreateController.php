b9e3a4f66f639ab2880fcd6238fa7685500d43e4
alex-skxy <alex13skx@gmail.com>
2021-06-09 14:59:13 +0200

    edit controller endpoint

--- app/Controllers/CreateController.php
+++ app/Controllers/CreateController.php
@@ -31,12 +31,12 @@ class CreateController
     {
         $pdo = db();
         $statement = $pdo->prepare("INSERT INTO loan (name, lastname, email, phone_number, installments, fk_creditpackage_id) VALUES (:name, :lastname, :phone_number, :phone_number, :installments, :creditpackage_id);");
-        $statement->bindParam(':name', $_POST['name']);
-        $statement->bindParam(':lastname', $_POST['lastname']);
-        $statement->bindParam(':email', $_POST['email']);
-        $statement->bindParam(':phone_number', $_POST['phone_number']);
-        $statement->bindParam(':installments', $_POST['installments']);
-        $statement->bindParam(':creditpackage_id', $_POST['creditpackage']);
+        $statement->bindParam(':name', $data['name']);
+        $statement->bindParam(':lastname', $data['lastname']);
+        $statement->bindParam(':email', $data['email']);
+        $statement->bindParam(':phone_number', $data['phone_number']);
+        $statement->bindParam(':installments', $data['installments']);
+        $statement->bindParam(':creditpackage_id', $data['creditpackage']);
 
         $statement->execute();
         $result = $statement->fetchAll();
--- app/Controllers/EditController.php
+++ app/Controllers/EditController.php
@@ -2,11 +2,78 @@
 
 class EditController
 {
+
     public function index()
     {
-        $hello = 'Edit';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->edit($_POST);
+        } else {
+            $exampleModel = new Example();
+            $creditpackageData = $exampleModel->getAll("creditpackage");
+            require 'app/Views/edit.view.php';
+        }
+    }
 
-        require 'app/Views/edit.view.php';
+
+    public function edit($data)
+    {
+        $validation_result = $this->validateData($data);
+        if (count($validation_result) > 0) {
+            http_response_code(422);
+            echo json_encode($validation_result);
+        } else {
+            $this->updateLoan($data);
+        }
+    }
+
+    public function updateLoan($data)
+    {
+        $pdo = db();
+        $statement = $pdo->prepare("UPDATE loan SET
+	name = :name,
+	lastname = :lastname,
+    email = :email,
+    phone_number = :phone_number,
+    fk_creditpackage_id = :creditpackage_id,
+    paid_back = :paid_back
+WHERE id_loan = :id;");
+        $statement->bindParam(':id', $data['id']);
+        $statement->bindParam(':name', $data['name']);
+        $statement->bindParam(':lastname', $data['lastname']);
+        $statement->bindParam(':email', $data['email']);
+        $statement->bindParam(':phone_number', $data['phone_number']);
+        $statement->bindParam(':creditpackage_id', $data['creditpackage']);
+        $paid_back = isset($data['paid_back']) ? 1 : 0;
+        $statement->bindParam(':paid_back', $paid_back);
+
+        $statement->execute();
+        $result = $statement->fetchAll();
+    }
+
+    public function validateData($data): array
+    {
+        $errors = [];
+        $simpletext_regex = "/^[\p{L} ]+$/";
+        $email_regex = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/";
+        $phone_regex = "/^[+0][\d ]+$/";
+
+        if (!(isset($data['name']) && preg_match($simpletext_regex, $data['name']))) {
+            $errors[] = "Name can't be empty or contain any numbers or special characters";
+        }
+        if (!(isset($data['lastname']) && preg_match($simpletext_regex, $data['lastname']))) {
+            $errors[] = "Lastname can't be empty or contain any numbers or special characters";
+        }
+        if (!(isset($data['email']) && preg_match($email_regex, $data['email']))) {
+            $errors[] = "Email can't be empty and must contain an @ and a domain name";
+        }
+        if (!(isset($data['phone_number']) && preg_match($phone_regex, $data['phone_number']))) {
+            $errors[] = "Phone number can't be empty and can only contain numbers, whitespace and a +";
+        }
+        if (!(isset($data['creditpackage']) && is_numeric($data['creditpackage']) && $data['creditpackage'] >= 0 && $data['creditpackage'] < 46)) {
+            $errors[] = "No such loan package";
+        }
+
+        return $errors;
     }
 }
 
